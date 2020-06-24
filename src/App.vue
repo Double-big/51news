e96cd6045e06b8e41b382923e140d9d22c6eef0d
dada <1113052033@qq.com>
2020-06-24 17:14:15 +0800

    全局基本样式

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+/* 在这里引入字体图标文件 */
+@import url("http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css");
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  color: inherit;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+ul,
+li,
+ol {
+  list-style: none;
+}
+
+.topNav + div {
+  padding-top: 16.111vw;
+}
+</style>

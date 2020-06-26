3b4d29157e605a4eb51291003d947de9e90c2b8e
dada <1113052033@qq.com>
2020-06-26 19:44:34 +0800

    vant-ui的安装和配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+
+//引入 vant-ui 组件库并配置
+// 全局引入
+import Vant from "vant";
+Vue.use(Vant);
+import "vant/lib/index.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  },
+}).$mount("#app");

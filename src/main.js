31475adbecd9932db416a96cc7b2f01e05b3b617
dada <1113052033@qq.com>
2020-06-26 20:30:18 +0800

    安装和配置axios库

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-
 //引入 vant-ui 组件库并配置
 // 全局引入
 import Vant from "vant";
 Vue.use(Vant);
 import "vant/lib/index.css";
+//引入axios库
+import axios from "axios";
+
+// 绑定到原型
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
 

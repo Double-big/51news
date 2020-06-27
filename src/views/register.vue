0beb752825c98a6e829bacc4d42968e4539a90b8
dada <1113052033@qq.com>
2020-06-27 11:41:45 +0800

    发送注册请求

--- src/views/register.vue
+++ src/views/register.vue
@@ -8,11 +8,29 @@
       <span class="iconfont iconnew"></span>
     </div>
 
-    <AuthInput type="text" placeholder="用户名/手机号" rule="^.{3,10}$" errMsg="请输入正确内容"></AuthInput>
-    <AuthInput type="nickname" placeholder="昵称" rule="^.{3,10}$" errMsg="请输入正确内容"></AuthInput>
-    <AuthInput type="password" placeholder="密码" rule="^.{3,10}$" errMsg="请输入正确内容"></AuthInput>
+    <AuthInput
+      type="text"
+      placeholder="用户名/手机号"
+      rule="^.{3,10}$"
+      errMsg="请输入正确内容"
+      @valueChange="setUsername"
+    ></AuthInput>
+    <AuthInput
+      type="nickname"
+      placeholder="昵称"
+      rule="^.{3,10}$"
+      errMsg="请输入正确内容"
+      @valueChange="setNickname"
+    ></AuthInput>
+    <AuthInput
+      type="password"
+      placeholder="密码"
+      rule="^.{3,10}$"
+      errMsg="请输入正确内容"
+      @valueChange="setPassword"
+    ></AuthInput>
 
-    <AuthBtn btnText="注册"></AuthBtn>
+    <AuthBtn btnText="注册" @clickBtn="register"></AuthBtn>
 
     <div class="bottom"></div>
   </div>
@@ -22,9 +40,59 @@
 import AuthInput from "@/components/AuthInput.vue";
 import AuthBtn from "@/components/AuthBtn.vue";
 export default {
+  data() {
+    return {
+      username: "",
+      nickname: "",
+      password: ""
+    };
+  },
   components: {
     AuthInput,
     AuthBtn
+  },
+  methods: {
+    setUsername(username) {
+      this.username = username;
+    },
+    setNickname(nickname) {
+      this.nickname = nickname;
+    },
+    setPassword(password) {
+      this.password = password;
+    },
+    register() {
+      if (!this.username || !this.nickname || !this.password) {
+        this.$toast("请填写完整信息");
+        return;
+      }
+      this.$axios({
+        url: "http://liangwei.tech:3000/register",
+        method: "post",
+        //获取 this 里面的 用户信息
+        data: {
+          username: this.username,
+          nickname: this.nickname,
+          password: this.password
+        }
+      })
+        .then(res => {
+          //   console.log("这里是成功处理");
+          const { statusCode, message } = res.data;
+          //   console.log(res.data);
+          if (statusCode) {
+            //有statusCode 则失败
+            this.$toast.fail(message);
+          } else {
+            //   没有statusCode 则成功
+            this.$toast.success(message);
+          }
+        })
+        .catch(err => {
+          //   console.log("这里是错误处理");
+          this.$toast.fail(message);
+        });
+    }
   }
 };
 </script>

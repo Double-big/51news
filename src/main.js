be7407540bab1c4312861a361b968584ee2c0586
dada <1113052033@qq.com>
2020-06-28 11:56:09 +0800

    设置基准路径

--- src/main.js
+++ src/main.js
@@ -7,11 +7,18 @@ import router from "./router";
 import Vant from "vant";
 Vue.use(Vant);
 import "vant/lib/index.css";
+
+
 //引入axios库
 import axios from "axios";
-
 // 绑定到原型
 Vue.prototype.$axios = axios;
+
+//设置基准路径
+//远程地址
+axios.defaults.baseURL = "http://liangwei.tech:3000";
+// 本地地址
+// axios.defaults.baseURL = "http://127.0.0.1:3000";
 
 Vue.config.productionTip = false;
 

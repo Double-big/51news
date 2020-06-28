746305bae944b860225a6bac918895c0c3555a63
dada <1113052033@qq.com>
2020-06-28 11:55:53 +0800

    退出清除token和user_id

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,25 @@
     <router-link to="/register">注册页</router-link>
     <br />
     <router-link to="/person">个人中心</router-link>
+    <br />
+    <button @click="logout">退出登录</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    logout() {
+      // const { data } = res.data;
+      localStorage.removeItem("token");
+      localStorage.removeItem("user_id");
+      
+      this.$router.replace("/login")
+      // localStorage.removeItem("token", data.token);
+      // localStorage.removeItem("user_id", data.user.id);
+    }
+  }
+};
 </script>
 
 <style>

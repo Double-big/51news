056f5829d225d5d99dd09ab0b72f656dee5cb6a0
dada <1113052033@qq.com>
2020-06-28 09:08:02 +0800

    tabBar组件的布局及样式

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    我的关注11111
-    <div></div>
+  <div class="tabbar">
+    <div class="left">{{leftText}}</div>
+    <div class="right">
+      {{rightText}}
+      <span class="iconfont iconjiantou1"></span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["leftText", "rightText"]
+};
 </script>
 
 <style lang="less" scoped>
+.tabbar {
+  display: flex;
+  border-bottom: 1px solid #c4c4c4;
+  line-height: 12.222vw;
+  // align-items: center;
+  margin-left: 25px;
+  font-size: 3.889vw;
+  .left {
+    display: flex;
+    flex: 1;
+  }
+  .right {
+    color: #c4c4c4;
+  }
+}
 </style>

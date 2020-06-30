d8f84a65d08d9453f341483c80f51d6173eb941a
dada <1113052033@qq.com>
2020-06-30 11:57:31 +0800

    编辑页头部公共导航组件

---
+++ src/components/TopNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="topNav">
+    <div class="arrow">
+      <span class="iconfont iconjiantou"></span>
+    </div>
+    <span class="title">{{title}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"]
+};
+</script>
+
+<style lang="less" scoped>
+.topNav {
+  display: flex;
+  height: 60px;
+  align-items: center;
+  padding: 0 5.556vw;
+  .title {
+    flex: 1;
+    text-align: center;
+    font-weight: 700;
+    font-size: 3.889vw;
+    color: #333;
+    position: relative;
+    right: 15px;
+  }
+}
+</style>

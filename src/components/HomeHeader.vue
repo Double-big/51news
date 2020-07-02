06125ab5357295ae9b304129f426b73b5cfbb921
dada <1113052033@qq.com>
2020-07-02 12:04:12 +0800

    主页头部组件

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <span class="iconfont iconnew"></span>
+    <div class="search">
+      <span class="iconfont iconsearch">搜索新闻</span>
+    </div>
+    <div class="iconfont iconwode"></div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+<style lang="less" scoped>
+.header {
+  display: flex;
+  padding: 0 10px;
+  color: #fff;
+  align-items: center;
+  background-color: red;
+  .iconnew {
+    font-size: 54px;
+  }
+  .search {
+    flex: 1;
+    width: 210px;
+    height: 34px;
+    line-height: 34px;
+    background-color: rgba(255, 255, 255, 0.6);
+    text-align: center;
+    margin: 0 25px 0 10px;
+    border-radius: 15px;
+  }
+  .iconwode {
+    font-size: 32px;
+  }
+}
+</style>

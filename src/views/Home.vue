ebeeb48bf6c7a9194c23411f478ed5b122a146a8
dada <1113052033@qq.com>
2020-07-02 12:03:45 +0800

    获取栏目,  获取当前激活分类文章

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,57 @@
 <template>
   <div>
-    <!-- 这里是首页 -->
-    <router-link to="/login">登录页</router-link>
-    <br />
-    <router-link to="/register">注册页</router-link>
-    <br />
-    <router-link to="/person">个人中心</router-link>
-    <br />
-    <button @click="logout">退出登录</button>
+    <HomeHeader />
+    <van-tabs v-model="active">
+      <van-tab v-for="item in categoriesList" :key="item.id" :title="item.name">{{item.name}}</van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
+import HomeHeader from "@/components/HomeHeader";
 export default {
+  components: {
+    HomeHeader
+  },
+  data() {
+    return {
+      active: 0,
+      categoriesList: []
+      // categoryId: []
+    };
+  },
+  created() {
+    this.$axios({
+      url: "/category"
+    }).then(res => {
+      this.categoriesList = res.data.data;
+      //获取当前文章, 发送ajax请求. 封装 getPost()
+      this.getPost();
+    });
+  },
   methods: {
-    logout() {
-      // const { data } = res.data;
-      localStorage.removeItem("token");
-      localStorage.removeItem("user_id");
-      
-      this.$router.replace("/login")
-      // localStorage.removeItem("token", data.token);
-      // localStorage.removeItem("user_id", data.user.id);
+    getPost() {
+      //栏目列表
+      this.categoriesList;
+      //当前激活栏目的索引
+      this.active;
+      // 可以拿到当前激活栏目
+      this.categoriesList[this.active];
+      // 获取id
+      this.categoriesList[this.active].id;
+      //当前激活分类, 是在分类列表中拿出当前激活的索引
+      const currentCategory = this.categoriesList[this.active];
+      //从中获取 id
+      const categoryId = currentCategory.id;
+      this.$axios({
+        url: "/post",
+        params: {
+          category: categoryId
+        }
+      }).then(res => {
+        // this.categoryId = this.categoriesList[this.active].id;
+        console.log(res.data);
+      });
     }
   }
 };
